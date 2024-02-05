08e4abfb1e388d2970e0433aa9f19109f8ee5831
farhan maulana <farhanmaulana1710@gmail.com>
2024-02-05 11:28:43 +0700

    update ui

--- src/components/Totop.jsx
+++ src/components/Totop.jsx
@@ -21,8 +21,8 @@ export default function Totop() {
 
     return (
         <>
-            <a href="#" className={`fixed bottom-4 right-4 z-[9999]  ${toTop ? 'flex' : 'hidden'} h-14 w-14 items-center justify-center rounded-full bg-primary p-4 transition duration-500 hover:animate-bounce `} id={'to-top'}>
-                <span className={'mt-2 block h-5 w-5 rotate-45 border-t-2 border-l-2'}></span>
+            <a href="#" className={`fixed bottom-4 right-4 z-[9999]  ${toTop ? 'flex' : 'hidden'} h-14 w-14 items-center justify-center rounded-full bg-primary p-4 transition duration-500 animate-bounce dark:bg-white `} id={'to-top'}>
+                <span className={'mt-2 block h-5 w-5 rotate-45 border-t-2 border-l-2 dark:border-dark'}></span>
             </a>
         </>
     )
