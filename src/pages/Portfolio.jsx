ac62ce1c8454cbbce5040608fc010c78b80a669f
farhan maulana <farhanmaulana1710@gmail.com>
2024-06-30 00:30:14 +0700

    update portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,9 +3,9 @@ export default function Portfolio() {
         <section id="portfolio" className={'pt-36 pb-32 bg-slate-100 dark:bg-[#CCCCCC]'}>
             <div className="container">
                 <div className="w-full px-4">
-                    <div className="mx-auto text-center mb-16" 
-                    data-aos="zoom-in"
->
+                    <div className="mx-auto text-center mb-16"
+                        data-aos="zoom-in"
+                    >
                         <h4 className={'text-lg text-primary font-semibold mb-2 dark:text-[#1A1A1A] uppercase'}>Portfolio</h4>
                         <h2 className="font-bold text-dark text-3xl mb-4 sm:text-4xl lg:text-5xl dark:text-[#1A1A1A]">Project Saya</h2>
                         <p className="text-md font-medium text-secondary md:text-lg dark:text-[#1A1A1A]">Sebuah project saya, yang selalu
@@ -13,9 +13,9 @@ export default function Portfolio() {
                             kembangkan</p>
                     </div>
                     <div className="w-full p-4 flex flex-wrap justify-center xl:w-10/12 xl:mx-auto ">
-                        <div className="mb-12 p-4 md:w-1/3" 
-                        data-aos="zoom-out-right"
-                        
+                        <div className="mb-12 p-4 md:w-1/3"
+                            data-aos="zoom-out-right"
+
                         >
                             <div className="rounded-md shadow-md overflow-hidden">
                                 <img src="/img/portfolio/1.png" alt="jokes-random" width={'w-full'}
@@ -26,9 +26,9 @@ export default function Portfolio() {
                                     rel="noreferrer" className={'dark:text-[#1A1A1A] dark:hover:text-[#333333] transition duration-300'}>JoRan (jokes random)</a></h3>
                             <p className={'font-medium text-base text-secondary dark:text-[#1A1A1A]'}>Sebuah project menggunakan tools logika react js</p>
                         </div>
-                        <div className="mb-12 p-4 md:w-1/3" 
-                        data-aos="zoom-in"
-                        
+                        <div className="mb-12 p-4 md:w-1/3"
+                            data-aos="zoom-in"
+
                         >
                             <div className="rounded-md shadow-md overflow-hidden">
                                 <img src="/img/portfolio/2.png" alt="todo apps" width={'w-full'}
@@ -40,9 +40,9 @@ export default function Portfolio() {
                             <p className={'font-medium text-base text-secondary dark:text-[#1A1A1A]'}>Sebuah project react, dengan
                                 logika todo</p>
                         </div>
-                        <div className="mb-12 p-4 md:w-1/3"  
-                        data-aos="zoom-out-left"
-                        
+                        <div className="mb-12 p-4 md:w-1/3"
+                            data-aos="zoom-out-left"
+
                         >
                             <div className="rounded-md shadow-md overflow-hidden">
                                 <img src="/img/portfolio/3.png" alt="notes app" width={'w-full'}
@@ -56,9 +56,9 @@ export default function Portfolio() {
                             <p className={'font-medium text-base text-secondary dark:text-[#1A1A1A]'}>Sebuah project dengan logika
                                 react</p>
                         </div>
-                        <div className="mb-12 p-4 md:w-1/3" 
-                        data-aos="zoom-out-right"
-                        
+                        <div className="mb-12 p-4 md:w-1/3"
+                            data-aos="zoom-out-right"
+
                         >
                             <div className="rounded-md shadow-md overflow-hidden">
                                 <img src="/img/portfolio/4.png" alt="wisata id" width={'w-full'}
@@ -70,9 +70,9 @@ export default function Portfolio() {
                             <p className={'font-medium text-base text-secondary dark:text-[#1A1A1A]'}>Sebuah project team dengan
                                 react,express.js, yang sangat kompleks</p>
                         </div>
-                        <div className="mb-12 p-4 md:w-1/3" 
-                        data-aos="zoom-in"
-                        
+                        <div className="mb-12 p-4 md:w-1/3"
+                            data-aos="zoom-in"
+
                         >
                             <div className="rounded-md shadow-md overflow-hidden">
                                 <img src="/img/portfolio/5.png" alt="Yok ngopi" width={'w-full'}
@@ -83,18 +83,27 @@ export default function Portfolio() {
                                     rel="noreferrer" className={'dark:text-[#1A1A1A] dark:hover:text-[#333333] transition duration-300'}>Yok Ngopi</a></h3>
                             <p className={'font-medium text-base text-secondary dark:text-[#1A1A1A]'}>Sebuah project yang menggunakan vanila language,html css javascript</p>
                         </div>
-                        <div className="mb-12 p-4 md:w-1/3" 
-                        data-aos="zoom-out-left"
-                        
-                        >
+                        <div className="mb-12 p-4 md:w-1/3"
+                            data-aos="zoom-out-left">
                             <div className="rounded-md shadow-md overflow-hidden">
                                 <img src="/img/portfolio/6.png" alt="Yok ngopi" width={'w-full'}
-                                    title={'Yok ngopi'} />
+                                    title={'Dekstop Chat Api'} />
                             </div>
                             <h3 className={'font-semibold text-xl text-[#545353] mb-3 mt-5 transition duration-500 hover:text-primary'}>
                                 <a href="https://github.com/farhnDev/chat-api-MERN.git" target={"_blank"} title={'visit github project'}
                                     rel="noreferrer" className={'dark:text-[#1A1A1A] dark:hover:text-[#333333] transition duration-300'}>Web Chat Clone</a></h3>
                             <p className={'font-medium text-base text-secondary dark:text-[#1A1A1A]'}>Sebuah project yang di kembangkan dengan MERN Stack, yang bisa digunakan secara online</p>
+                        </div>
+                        <div className="mb-12 p-4 md:w-1/3"
+                            data-aos="zoom-out-right">
+                            <div className="rounded-md shadow-md overflow-hidden">
+                                <img src="/img/portfolio/7.png" alt="Yok ngopi" width={'w-full'}
+                                    title={'Beasiswa Finder'} />
+                            </div>
+                            <h3 className={'font-semibold text-xl text-[#545353] mb-3 mt-5 transition duration-500 hover:text-primary'}>
+                                <a href="https://github.com/farhnDev/beasiswa-finder.git" target={"_blank"} title={'visit github project'}
+                                    rel="noreferrer" className={'dark:text-[#1A1A1A] dark:hover:text-[#333333] transition duration-300'}>Beasiswa Finder</a></h3>
+                            <p className={'font-medium text-base text-secondary dark:text-[#1A1A1A]'}>Sebuah project yang di kembangkan dengan laravel dan juga dengan MySQL. sebagai project team</p>
                         </div>
                     </div>
                 </div>
